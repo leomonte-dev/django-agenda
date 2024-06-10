8d61f72988d7e3945e2f7f11b79661f5ea70b8dc
Leonardo Monte <planecta@gmail.com>
2024-06-10 18:55:52 -0300

    Changing settings

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# id (primary key)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (image)
+
+# class Contact(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     phone = models.CharField(max_length=20)
+#     email = models.EmailField()
+#     created_date = models.DateField()
+#     description = models.TextField()
+#     show = models.BooleanField()
+#     picture = models.ImageField(upload_to='contacts')
+#     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+#     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.first_name
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
